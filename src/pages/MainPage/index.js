6f85fd44d9b635274a5a2831517dcf1a19a22126
shlee9999 <oklshop555@naver.com>
2023-05-01 20:28:53 +0900

    Feat: Navigation List

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -13,7 +13,10 @@ function App() {
             src={process.env.PUBLIC_URL + "images/header_logo.png"}
             className="header_logo"
           />
-          <p className="nav">nav</p>
+          <div className="nav">
+            <a href="#paragraph_1">About us</a>
+            <a href="#paragraph_2">Proposal</a>
+          </div>
         </header>
         <section>
           <div className="center_container">
@@ -31,11 +34,11 @@ function App() {
             </button>
           </div>
           <div className="paragraph_container">
-            <div className="paragraph_title">
+            <div className="paragraph_title" id="paragraph_1">
               <p>About us</p>
             </div>
             <div className="paragraph_text"></div>
-            <div className="paragraph_title">
+            <div className="paragraph_title" id="paragraph_2">
               <p>Proposal</p>
             </div>
             <div className="paragraph_text"></div>
