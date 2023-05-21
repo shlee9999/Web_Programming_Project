27a615f624be2f55c48314acfb5ae525608e5c8b
minjeong073 <kim.mj80730@gmail.com>
2023-05-21 14:41:17 +0900

    Style: modal button 스타일 수정
    - 한/영 버튼, start 버튼에 hover, active 스타일 추가

--- src/components/SelectCategoryModal/index.jsx
+++ src/components/SelectCategoryModal/index.jsx
@@ -90,13 +90,13 @@ const Modal = ({
         </div>
         <div className='select_language'>
           <p
-            className={`select_language_item ${language && 'active'}`}
+            className={`select_language_item ${language && 'item_active'}`}
             onClick={toKorean}
           >
             한글
           </p>
           <p
-            className={`select_language_item ${!language && 'active'}`}
+            className={`select_language_item ${!language && 'item_active'}`}
             onClick={toEnglish}
           >
             English
@@ -106,9 +106,8 @@ const Modal = ({
           {sentence_total.sentence.map((item, index) => {
             return (
               <button
-                className={`category_item ${
-                  index === sentenceIndex && 'select_sentence'
-                }`}
+                className={`category_item ${index === sentenceIndex && 'select_sentence'
+                  }`}
                 key={`${language}_category_${index}`}
                 onFocus={handleFocusCategory(item, index)}
                 onKeyDown={handleKeyDown}
