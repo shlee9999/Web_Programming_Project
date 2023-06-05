41dc85107f13e9358f4921932c893c3068457625
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-06-05 13:18:34 +0900

    Feat: 산성비 구현 but 비동기 에러 존재

--- src/components/Word/index.jsx
+++ src/components/Word/index.jsx
@@ -1,50 +1,100 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './index.css';
 
-export const Word = ({ item, order, isChecked }) => {
-  const [isAlive, setIsAlive] = useState(false);
+const baseStyle = {
+  width: '100px',
+  margin: '0 10px',
+};
+
+const non_movingStyle = {
+  ...baseStyle,
+  opacity: 0,
+};
+
+// Helper function to create a delay
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const Word = ({
+  word,
+  isChecked,
+  timeLimit,
+  interval,
+  replaceCurrentWordList,
+}) => {
+  const [isFalling, setIsFalling] = useState(false);
   const [isTyped, setIsTyped] = useState(false);
   const [position, setPosition] = useState(0);
-  const interval = 1000;
-  const limit = 10; //떨어지는 데 걸리는 시간(초기 버튼 클릭으로 받아올듯)
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsAlive(true);
-      setPosition(70); //렌더링되면서 이동 시작!
-      setTimeout(() => {
-        setIsAlive(false);
-      }, limit * 1000); //도착 후엔 더이상입력불가능
-    }, order * interval); //index 순서대로 1초 간격으로 떨어짐
-  }, [order]);
 
   const movingStyle = {
+    ...baseStyle,
     transform: `translateY(${position}vh)`,
-    transition: `transform ${limit}s linear`,
     backgroundColor: 'skyblue',
-    width: '100px',
-    margin: '0 10px',
+    opacity: position === 0 ? 0 : 1,
+    transition: `opacity 0.5s, transform ${timeLimit}s linear`,
   };
-  const non_movingStyle = {
-    width: '100px',
-    margin: '0 10px',
-  };
+
+  const startFalling = useCallback(async () => {
+    try {
+      await delay(interval * 1000);
+      setIsFalling(true);
+      setPosition(70);
+    } catch (err) {
+      console.error(err);
+    }
+  }, [interval]);
+
+  const endFalling = useCallback(async () => {
+    try {
+      await delay(timeLimit * 1000);
+      setIsFalling(false);
+    } catch (err) {
+      console.error(err);
+    }
+  }, [timeLimit]);
+
+  const checkFallingWords = useCallback(async () => {
+    try {
+      await startFalling();
+      await endFalling();
+      if (position === 0) {
+        await delay(timeLimit * 1000);
+        checkFallingWords();
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }, [position, timeLimit, endFalling, startFalling]);
+
+  useEffect(() => {
+    checkFallingWords();
+  }, []);
+
   useEffect(() => {
     if (isTyped) {
       setPosition(0);
+      setIsTyped(false);
+      setIsFalling(false);
+      delay(1000).then(() => {
+        replaceCurrentWordList(word);
+      });
     }
-  }, [isTyped]);
+  }, [isTyped, replaceCurrentWordList, word]);
 
   useEffect(() => {
-    if (isChecked && isAlive) setIsTyped(true);
-  }, [isChecked, isAlive]);
+    if (isChecked && isFalling) {
+      setIsTyped(true);
+      delay(0).then(() => {
+        throw new Error(word + ' 성공');
+      });
+    }
+  }, [isChecked, word, isFalling]);
 
   return (
     <div className='word'>
       {isTyped ? (
         <div style={non_movingStyle}></div>
       ) : (
-        <div style={movingStyle}>{item}</div>
+        <div style={movingStyle}>{word}</div>
       )}
     </div>
   );
