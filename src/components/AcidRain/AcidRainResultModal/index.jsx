a5daa45dcfaa62dbc528ddb7c2b4bbb6acecee54
KIM MINJEONG <91234288+minjeong073@users.noreply.github.com>
2023-06-20 18:23:16 +0900

    Refactor: AcidRainModal에 statistic button 추가

--- src/components/AcidRain/AcidRainResultModal/index.jsx
+++ src/components/AcidRain/AcidRainResultModal/index.jsx
@@ -1,13 +1,19 @@
 import './index.css';
 
-const AcidRainResultModal = ({ closeModal, score }) => {
+const AcidRainResultModal = ({ closeModal, score, openStatisticsModal }) => {
   const handleClickModal = (e) => {
     e.stopPropagation();
   };
   return (
     <div className='modal_overlay' onClick={closeModal}>
-      <div className='modal' onClick={handleClickModal}>
+      <div className='result_modal' onClick={handleClickModal}>
         <h2>Correct Words : {score}</h2>
+        <button
+          className='acid_statistics_button'
+          onClick={openStatisticsModal}
+        >
+          My Statistics
+        </button>
       </div>
     </div>
   );
